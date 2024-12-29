e58f7ab4b11c44a9ee5683383be3eaa47a3b0cc3
PC <git@github.com:denis-poroshin/store1.git>
2024-12-29 16:08:56 +0300

    Начало

---
+++ src/main/java/ru/test/mail/test_mail_ru_and_kafka/TestMailRuAndKafkaApplication.java
@@ -0,0 +1,13 @@
+package ru.test.mail.test_mail_ru_and_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMailRuAndKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMailRuAndKafkaApplication.class, args);
+	}
+
+}
